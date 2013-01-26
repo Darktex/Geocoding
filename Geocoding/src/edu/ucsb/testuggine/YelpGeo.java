b98c81dae09739968ce83ab7a8f9940a4653da53
Davide Testuggine <darktex@gmail.com>
2013-01-25 22:46:58 -0800

    Almost done for Yelp

--- Geocoding/src/edu/ucsb/testuggine/YelpGeo.java
+++ Geocoding/src/edu/ucsb/testuggine/YelpGeo.java
@@ -5,53 +5,127 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 
 public class YelpGeo {
 	JSONParser parser;
 	MySQLConnection db;
-	
-	
-	public YelpGeo() throws SQLException {
-		String selectionQuery = "SELECT id, " +
-				"addressNum, addressStreet, addressCity, addressRegion, addressZip" +
-				" FROM  YelpRestaurant;";
+
+	public YelpGeo() throws SQLException, ParseException {
+		parser = new JSONParser();
+		db = new MySQLConnection();
+
+		String selectionQuery = "SELECT id, "
+				+ "addressNum, addressStreet, addressCity, addressRegion, addressZip"
+				+ " FROM  YelpRestaurant;";
 		PreparedStatement checkStatement = db.con
 				.prepareStatement(selectionQuery);
 		ResultSet res = checkStatement.executeQuery();
-		
+
 		while (res.next()) {
 			String id = res.getString("id");
 			String rawData = getGoogleJSON(res);
 			String[] coords = processJSON(rawData);
-			
+
+			String insertionQuery = "INSERT INTO `YelpCoords` (`restaurant_id`, `latitude`, "
+					+ "`longitude`) " + "VALUES (?, ?, ?);";
+			PreparedStatement prep = db.con
+					.prepareStatement(insertionQuery);
+			if (!alreadyThere(id)) {	
+				prep.setString(1, id);
+				prep.setString(2, coords[0]);
+				prep.setString(3, coords[1]);
+				prep.execute();
+			}
+			prep.close();
 		}
-		
-		String insertionQuery = "INSERT INTO `YelpCoords` (`restaurant_id`, `latitude`, " +
-				"`longitude`) " +
-				"VALUES (?, ?, ?);";
 	}
-	
-	private String[] processJSON(String rawData) {
-		// TODO Auto-generated method stub
-		return null;
+
+	private boolean alreadyThere(String id) throws SQLException {
+		String alreadyExistsCheckQuery = "SELECT * FROM  `YelpCoords` WHERE  `id` =  ?";
+		PreparedStatement checkStatement = db.con
+				.prepareStatement(alreadyExistsCheckQuery);
+		checkStatement.setString(1, id); // the ID of this restaurant
+		ResultSet alreadyExistsRes = checkStatement.executeQuery();
+		if (!alreadyExistsRes.first() ) return false;
+		return true;
+	}
+
+	private String[] processJSON(String rawData) throws ParseException {
+		String[] result = new String[2];
+
+		Object allDataObj = parser.parse(rawData);
+		JSONObject jsonObject = (JSONObject) allDataObj;
+		// The results are so structured:
+		// Everything is in a 2-slots array, where
+		// [1] contains metadata and [0] contains data
+
+		JSONArray resultsArray = (JSONArray) jsonObject.get("results");
+		Object metaObj = resultsArray.get(1);
+		Object dataObj = resultsArray.get(0);
+
+		JSONObject metaData = (JSONObject) metaObj;
+
+		String status = (String) metaData.get("status");
+		// Status Codes
+
+		// The "status" field within the Geocoding response object contains the
+		// status of the request, and may contain debugging information to help
+		// you track down why Geocoding is not working. The "status" field may
+		// contain the following values:
+
+		// "OK" indicates that no errors occurred; the address was successfully
+		// parsed and at least one geocode was returned.
+		// "ZERO_RESULTS" indicates that the geocode was successful but returned
+		// no results. This may occur if the geocode was passed a non-existent
+		// address or a latlng in a remote location.
+		// "OVER_QUERY_LIMIT" indicates that you are over your quota.
+		// "REQUEST_DENIED" indicates that your request was denied, generally
+		// because of lack of a sensor parameter.
+		// "INVALID_REQUEST" generally indicates that the query (address or
+		// latlng) is missing.
+
+		JSONObject data = (JSONObject) dataObj;
+
+		// The data we are interested in (geocode) lies in
+		// "geometry" -> "location". This one has 2 elements (not an array tho)
+		// "lat" and "lng".
+
+		JSONObject geometry = (JSONObject) data.get("geometry");
+		JSONObject location = (JSONObject) geometry.get("location");
+
+		String latitude = (String) location.get("lat");
+		String longitude = (String) location.get("lng");
+
+		result[0] = latitude;
+		result[1] = longitude;
+
+		return result;
 	}
 
 	private String getGoogleJSON(ResultSet res) throws SQLException {
-		String addressNum = res.getString("addressNum").trim().replaceAll("/s", "+");
-		String addressStreet = res.getString("addressStreet").trim().replaceAll("/s", "+");
-		String addressCity = res.getString("addressCity").trim().replaceAll("/s", "+");
-		String addressRegion = res.getString("addressRegion").trim().replaceAll("/s", "+");
-		String addressZip = res.getString("addressZip").trim().replaceAll("/s", "+");
-		
+		String addressNum = res.getString("addressNum").trim()
+				.replaceAll("/s", "+");
+		String addressStreet = res.getString("addressStreet").trim()
+				.replaceAll("/s", "+");
+		String addressCity = res.getString("addressCity").trim()
+				.replaceAll("/s", "+");
+		String addressRegion = res.getString("addressRegion").trim()
+				.replaceAll("/s", "+");
+		String addressZip = res.getString("addressZip").trim()
+				.replaceAll("/s", "+");
+
 		String prefix = "http://maps.googleapis.com/maps/api/geocode/json?address=";
 		String postfix = "&sensor=false";
-		
-		String geoString = addressNum + "+" + addressStreet + ",+" + addressCity + ",+" + 
-				addressRegion + "+" + addressZip;
-		
+
+		String geoString = addressNum + "+" + addressStreet + ",+"
+				+ addressCity + ",+" + addressRegion + "+" + addressZip;
+
 		Document doc = getHTMLFromPage(geoString);
 		String result = doc.select("pre").first().ownText();
 		return result;
@@ -71,7 +145,8 @@ public class YelpGeo {
 				success = true;
 				break;
 			} catch (IOException e) {
-				System.out.println("Failed to connect to " + url + "\n\tRetrying...");
+				System.out.println("Failed to connect to " + url
+						+ "\n\tRetrying...");
 			}
 		}
 		return doc;
